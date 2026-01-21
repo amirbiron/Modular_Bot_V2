68a75d229bc0df5b1ccb49e567197fa277761a4c
amir haim <amirbiron@gmail.com>
2026-01-22 01:17:50 +0200

    Delete github_manager.py

--- github_manager.py
+++
@@ -1,9 +0,0 @@
-def get_dashboard_widget():
-    return {
-        "title": "GitHub Backup",
-        "value": "Waiting...",
-        "label": "Click to backup project",
-        "status": "warning",  # צבע צהוב
-        "icon": "bi-github",
-        "link": "/backup"
-    }
